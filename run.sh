baf195daec9f33393a9680f4b6ad4a8791994d4f
jtloong <joshua.t.loong@gmail.com>
2018-08-07 19:06:09 -0400

    Edited readme and bash script for better setup

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
-#npm start
 cd sg2im-service
 export FLASK_APP=app.py
 source env/bin/activate
 pip install -r requirements.txt
 echo $PWD > env/lib/python3.5/site-packages/sg2im.pth
-python app.py
+
+if [ -d sg2im-models ];
+then
+  python app.py
+else
+  mkdir -p sg2im-models
+  wget https://storage.googleapis.com/sg2im-data/small/vg128.pt -O sg2im-models/vg128.pt
+fi
